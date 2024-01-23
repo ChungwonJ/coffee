9c520cf6d3abc6e6a5f87f4fd88e530e2127c48c
Chungwon <wjdcjddnjs1234@gmail.com>
2024-01-23 13:06:11 +0900

    기능완료

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '@/styles/globals.scss'
 import { Provider } from 'react-redux';
 import store from '@/redux/Store';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './Header';
+import Header from '@/componenents/Header';
 
 export default function App({ Component, pageProps }) {
   return (
